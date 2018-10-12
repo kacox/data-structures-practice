978cdf8d9e3aa05108dfff51a68af96ed7135258
Kasey <ka.cox@outlook.com>
2018-10-12 11:39:48 -0700

    Complete DLL add and __repr__ methods

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -9,9 +9,7 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return "<Previous: {}>--<Node: {}>--<Next: {}>".format(self.previous,
-                                                                self.data,
-                                                                self.next)
+        return "<Node: {}>".format(self.data)
 
 class DoublyLinkedList:
     """A Doubly Linked List (DLL)."""
@@ -21,8 +19,18 @@ class DoublyLinkedList:
         self.tail = None
 
     def add(self, data):
-        """Add a node to the DLL."""
-        pass
+        """Add a node to the end of the DLL."""
+        if not self.head:
+            first_node = Node(data)
+            self.head = first_node
+            self.tail = first_node
+        else:
+            current = self.head
+            while current.next:
+                # traverse to the last node
+                current = current.next
+            current.next = Node(data)
+        
 
     def insert(self, data, new_data):
         """Insert a node before a given node."""
@@ -33,5 +41,23 @@ class DoublyLinkedList:
         pass
 
     def __repr__(self):
-        # Traverse the DLL
-        pass
+        """Human readible version of the DLL."""
+        if self.head:
+            ans = str(self.head.data)
+            current = self.head
+            while current.next:
+                ans = " <--> ".join([ans, str(current.next.data)])
+                current = current.next
+            return "[" + ans + "]"
+        else:
+            return "Empty DLL"
+
+
+if __name__ == '__main__':
+    # Make and test the DLL
+    dll = DoublyLinkedList()
+    dll.add(6)
+    dll.add(2)
+    dll.add(12)
+    dll.add(9)
+    dll.add(22)
