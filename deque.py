3f319f75f8c353c7b3df1270a6eaf00af2992c14
Kasey <ka.cox@outlook.com>
2018-10-17 18:26:01 -0700

    Complete Deque implementation

--- deque.py
+++ deque.py
@@ -32,34 +32,51 @@ Algorithms and Data Structures using Python" by Brad Miller and David Ranum,
 Luther College
 """
 
+import doubly_linked_list as dll
+
 
 class Deque:
     """f"""
 
     def __init__(self):
         self.size = 0
-        self.items = None # TODO: make a doubly-linked list to store items
+        self.items = dll.DoublyLinkedList()
 
     def add_front(self, item):
         """Add an item to the front of the deque."""
-        pass
+        self.items.insert(0, item)
+        self.size += 1
 
     def add_rear(self, item):
         """Add an item to the rear of the deque."""
-        pass
+        self.items.append(item)
+        self.size += 1
 
     def remove_front(self):
         """Remove an item from the front of the deque."""
-        pass
+        self.size -= 1
+        return self.items.remove(self.items.head.data)
 
     def remove_rear(self):
         """Remove an item from the rear of the deque."""
-        pass
+        self.size -= 1
+        return self.items.pop()
 
     def is_empty(self):
         """Check if the deque is empty."""
-        return self.size() == 0
+        return self.size == 0
 
-    def size(self):
+    def get_size(self):
         """Return the size of the deque."""
-        return self.size()
+        return self.size
+
+    def __repr__(self):
+        return self.items.__repr__()
+
+
+if __name__ == '__main__':
+    d = Deque()
+    d.add_front("a")
+    d.add_front("b")
+    d.add_rear("c")
+    print(d)
